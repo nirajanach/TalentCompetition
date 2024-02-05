8a1ffb636d0711b835e3c14042ae4f66723bf12a
Nirajan Acharya <nirajanach@gmail.com>
2024-02-05 23:07:50 +1000

    Add URLconstant

---
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/constants/URLStorage.js
@@ -0,0 +1,9 @@
+﻿const IDENTITY_URL = "https://talentcompetitionidentity.azurewebsites.net/";
+export const LOCAL_IDENTITY_URL = "http://localhost:60998/";
+
+export const PROFILE_URL = "https://talentcompetitionprofile.azurewebsites.net/";
+
+export const TALENT_URL = "https://talentcompetitiontalent.azurewebsites.net/";
+
+
+export default IDENTITY_URL;
